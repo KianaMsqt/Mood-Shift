68ca658521a3b234a40a98e6a03be9aafcfcdd7a
KianaMsqt <masqati.kiana@gmail.com>
2023-02-03 18:46:29 +0000

    -Add functionality of storing data on localstorage -Add function for formatting the date -Add functionality of showing datas on the page

--- assets/js/script.js
+++ assets/js/script.js
@@ -47,3 +47,37 @@ $('#happy').on('click', function () {
 
   $('div.to-show').show();
 });
+$( document ).ready( function() {
+
+    // Storing user's mood and date on local storage.
+    $( "#saveButton" ).click( function() {
+
+        // Storing current date
+        var date = new Date();
+        var text = $( "#textInput" ).val();
+
+        localStorage.setItem( "date", date );
+        localStorage.setItem( "text", text );
+
+    } );
+
+    // Format the date to Day dd/mm/yyyy hh:mm:ss
+    // eg: Thursday 01/01/1970 01:00:00
+    var formatDate = function() {
+        var date = new Date(localStorage.getItem("date"));
+        var formattedDate = date.toLocaleDateString();
+        var formattedTime = date.toLocaleTimeString();
+        var day = date.toLocaleString('default', { weekday: 'long' });
+        console.log(day + " " + formattedDate + " " + formattedTime);
+    }
+
+    // Show stored data on the page
+
+    // TODO: It would be on click if we want to show it on a modal 
+    // Or should be on document ready if we wwant to show it on another page. In this case we need to check if the elemnt is on the page or not, to prevent errors on the home page.
+    $( '#moodTracker' ).on( 'click', function( e ) {
+        e.preventDefault();
+        $( 'body' ).append( formatDate() );
+    });
+
+} );
