93491dec84b26de7e6803f5934c3f956b44da708
sdjhen <114769817+sdjhen@users.noreply.github.com>
2023-02-03 23:26:32 +0000

    added smooth scroll code and r emoved whitespace from html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,12 @@
-// Smooth scroll to card content
-
 // Homepage Button Event
 
 $('#unhappy').on('click', function () {
   // Change background colour on click
   $('.jumbo-style').css('background-color', '#39a0ca');
+
+  // Smooth scroll to card content
+  var contentReveal = document.querySelector('#card-reveal');
+  contentReveal.scrollIntoView();
 
   // Reveal content cards
   $('div.to-show').show();
@@ -12,20 +14,36 @@ $('#unhappy').on('click', function () {
 
 $('#sad').on('click', function () {
   $('.jumbo-style').css('background-color', '#f6f5f3');
+
+  var contentReveal = document.querySelector('#card-reveal');
+  contentReveal.scrollIntoView();
+
   $('div.to-show').show();
 });
 
 $('#normal').on('click', function () {
   $('.jumbo-style').css('background-color', ' #fa625f');
+
+  var contentReveal = document.querySelector('#card-reveal');
+  contentReveal.scrollIntoView();
+
   $('div.to-show').show();
 });
 
 $('#good').on('click', function () {
   $('.jumbo-style').css('background-color', '#feda6a');
+
+  var contentReveal = document.querySelector('#card-reveal');
+  contentReveal.scrollIntoView();
+
   $('div.to-show').show();
 });
 
 $('#happy').on('click', function () {
   $('.jumbo-style').css('background-color', '#DCC7AA');
+
+  var contentReveal = document.querySelector('#card-reveal');
+  contentReveal.scrollIntoView();
+
   $('div.to-show').show();
 });
