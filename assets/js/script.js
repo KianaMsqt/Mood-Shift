bba8e5573921a15288b1f51e1c700bd88ff3a8b4
KianaMsqt <masqati.kiana@gmail.com>
2023-02-06 14:21:45 +0000

    Store mood in local storage and show moodtracker on the page

--- assets/js/script.js
+++ assets/js/script.js
@@ -50,37 +50,51 @@ $('#happy').on('click', function () {
 
 $( document ).ready( function() {
 
-    // Storing user's mood and date on local storage.
-    $( ".btn-feeling" ).on( 'click', function() {
+  const buttons = $( ".btn-feeling" );
 
-        // Storing current date
-        var date = new Date();
-        var mood = $( this ).text();
+  buttons.click( function() {
 
-        localStorage.setItem( "date", date );
-        localStorage.setItem( "text", mood );
+    // Store mood in local storage via getting the text of the clicked button
+    const text = $( this ).text().trim();
+    
+    // Ccurrent date and time
+    const currentDate = new Date();
+    
+    // Format date
+    const options = { weekday: "long", year: "numeric", month: "long", day: "numeric" };
+    const formattedDate = currentDate.toLocaleDateString("default", options);
+    const dayOfWeek = currentDate.toLocaleString("default", { weekday: "long" });
+    const date = `${formattedDate}`;
+    const time = currentDate.toLocaleTimeString();
+    
+    // Store the day of the week, date, time, and text as a JSON object in the local storage
+    localStorage.setItem(currentDate, JSON.stringify({ dayOfWeek, date, time, text }));
 
+  } );
 
-    } );
-
-    // Format the date to Day dd/mm/yyyy hh:mm:ss
-    // eg: Thursday 01/01/1970 01:00:00
-    var formatDate = function() {
-        var date = new Date(localStorage.getItem( "date" ));
-        var formattedDate = date.toLocaleDateString();
-        var formattedTime = date.toLocaleTimeString();
-        var day = date.toLocaleString('default', { weekday: 'long' });
-        console.log(day + " " + formattedDate + " " + formattedTime);
-    }
 
     // Show stored data on the page
 
     // TODO: It would be on click if we want to show it on a modal 
     // Or should be on document ready if we wwant to show it on another page. In this case we need to check if the elemnt is on the page or not, to prevent errors on the home page.
     $( '#moodTracker' ).on( 'click', function( e ) {
-        e.preventDefault();
-        $( 'body' ).append( formatDate() );
-        $( 'body' ).append( localStorage.getItem( "mood" ) );
+
+      // Prevent redirecting page after clicking on the button
+      e.preventDefault();
+
+        const displayData = $( "#displayTracker" );
+
+        // Iterate over all the keys in the local storage, get the values associated with each key, and parse them from JSON using
+        for ( let i = 0; i < localStorage.length; i++ ) {
+          const key = localStorage.key( i );
+          const value = JSON.parse( localStorage.getItem( key ) );
+
+          const p = $( "<p>" ).html( `You were ${ value.text } at ${ value.dayOfWeek } ${ value.date } ${ value.time }.` );
+
+          displayData.append( p );
+
+        }
+
     });
 
 } );
