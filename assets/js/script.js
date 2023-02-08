57d73c3c4e6966a196e920f1d18ae30faefe0f91
KianaMsqt <masqati.kiana@gmail.com>
2023-02-08 14:04:10 +0000

    Fix bug on scrolldown animation

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,46 +6,46 @@ $('#unhappy').on('click', function () {
 
   // Smooth scroll to card content
   var contentReveal = document.querySelector('#card-reveal');
-  contentReveal.scrollIntoView();
-
   // Reveal content cards
   $('div.to-show').show();
+  contentReveal.scrollIntoView();
+
 });
 
 $('#sad').on('click', function () {
   $('.jumbo-style').css('background-color', '#f6f5f3');
 
   var contentReveal = document.querySelector('#card-reveal');
+  $('div.to-show').show();
   contentReveal.scrollIntoView();
 
-  $('div.to-show').show();
 });
 
 $('#normal').on('click', function () {
   $('.jumbo-style').css('background-color', ' #fa625f');
 
   var contentReveal = document.querySelector('#card-reveal');
+  $('div.to-show').show();
   contentReveal.scrollIntoView();
 
-  $('div.to-show').show();
 });
 
 $('#good').on('click', function () {
   $('.jumbo-style').css('background-color', '#feda6a');
 
   var contentReveal = document.querySelector('#card-reveal');
+  $('div.to-show').show();
   contentReveal.scrollIntoView();
 
-  $('div.to-show').show();
 });
 
 $('#happy').on('click', function () {
   $('.jumbo-style').css('background-color', '#DCC7AA');
 
   var contentReveal = document.querySelector('#card-reveal');
+  $('div.to-show').show();
   contentReveal.scrollIntoView();
 
-  $('div.to-show').show();
 });
 
 $( document ).ready( function() {
